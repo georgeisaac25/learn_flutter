37c9fce9a0b75539df9be9a639ae5263603ca5cd
George <george.globant@vipper.com>
2020-09-05 01:12:08 +0530

    Initial commit

---
+++ lib/day_one.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FourthWidget());
+
+class FirstWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Isaac",
+      textDirection: TextDirection.ltr,
+      style: new TextStyle(color: Colors.black),
+    );
+  }
+}
+
+class SecondWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: FirstWidget(),
+    );
+  }
+}
+
+class ThirdWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("day_one"),
+      ),
+      body: SecondWidget(),
+      backgroundColor: Colors.lightBlueAccent,
+    );
+  }
+}
+
+class FourthWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "OnAppSelectionAndroid",
+      debugShowCheckedModeBanner: false,
+      home: ThirdWidget(),
+    );
+  }
+}
